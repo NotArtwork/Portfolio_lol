7607e9f7e2e29b43caf2fa9d0026a3cf70695260
NotArtwork <AntArce3@gmail.com>
2023-01-17 13:44:09 -0500

    nah homie

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,6 +4,18 @@ const Hero = () => {
     return(
         <div>
 
+            <h1> Antonio Arce</h1>
+            <ul>
+                Click Me
+                <li> Projects </li>
+                <li> About </li>
+            </ul>
+            <video>
+                <video>
+
+                </video>
+            </video>
+        
         </div>
     )
 }
